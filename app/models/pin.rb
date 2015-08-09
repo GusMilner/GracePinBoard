ea8d1a8675842880ecf807c012de68eb20a2c373
GusMilner <gusmilner@gmail.com>
2015-08-08 20:49:26 -0400

    Hoping AWS image upload works

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,14 +3,14 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, styles: { medium: "300x300>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	:url  => ""
-    :path => "public/avatars/:id/:style_:basename.:extension"
-    :storage => :fog
-    :fog_credentials => {
-        provider: 'AWS',
-        aws_access_key_id: "AKIAJOY7DTGLXRUCFGCQ",
-        aws_secret_access_key: "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"
-    }
-    fog_directory: "gracepinboard"
+	#:url  => ""
+    #:path => "public/avatars/:id/:style_:basename.:extension"
+    #:storage => :fog
+    #:fog_credentials => {
+       # provider: 'AWS',
+        #aws_access_key_id: "AKIAJOY7DTGLXRUCFGCQ",
+       # aws_secret_access_key: "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"
+    #}
+    #fog_directory: "gracepinboard"
 
 end
